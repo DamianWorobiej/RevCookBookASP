4cb5905b01a971aa998cc0c25cb2c7354711dd12
DamianWorobiej <damian.worobiej@gmail.com>
2019-06-30 18:21:42 +0200

    Dodaj pliki projektów.

---
+++ RevCookBookASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RevCookBookASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
